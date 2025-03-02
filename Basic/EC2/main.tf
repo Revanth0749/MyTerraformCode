04831f9b53da73b9b52c343f3f84856345f03df3
Revanth0749 <64368519+Revanth0749@users.noreply.github.com>
2025-03-02 22:35:16 +0530

    Update main.tf

--- Basic/EC2/main.tf
+++ Basic/EC2/main.tf
@@ -12,7 +12,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "terraform-ec2" {
-  ami           = "ami-0c55b159cbfafe1f0"
+  ami           = "ami-00bb6a80f01f03502"
   instance_type = "t2.micro"
   key_name = "ec2-key"
   associate_public_ip_address = true
