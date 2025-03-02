53717219a193733da947c26b65d55043b6dc0829
Revanth0749 <64368519+Revanth0749@users.noreply.github.com>
2025-03-02 22:26:32 +0530

    Main.tf for creating EC2

--- Basic/EC2/main.tf
+++ Basic/EC2/main.tf
@@ -1,1 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
 
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "terraform-ec2" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  key_name = "ec2-key"
+  associate_public_ip_address = true
+  tags = {
+    Name = "terraform-ec2"
+  }
+}
