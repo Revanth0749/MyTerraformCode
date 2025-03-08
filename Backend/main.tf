9b098b0f58af045c17453dd198c13403d2123fb9
Revanth0749 <64368519+Revanth0749@users.noreply.github.com>
2025-03-08 08:24:29 +0530

    Create main.tf for Terraform Backend

---
+++ Backend/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~>5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_s3_bucket" "tf-state" {
+  bucket = "tf-state-bucket-DemoEnv"
+  tags = {
+    Name = "Terraform-state-bucket"
+    Environment = "Demo"
+  }
+}
+
+resource "aws-aws_s3_bucket_versioning" "tf-state-version" {
+  bucket = aws_s3_bucket.tf-state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
